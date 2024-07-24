27a1bf9fc000cf96a03f7c687ef9b6291bd4e7e2
Fresasconcrem <chocorol390@gmail.com>
2024-07-24 23:49:57 +0100

    Add files via upload

---
+++ pre.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function() {
+  var container = document.querySelector('.container');
+  container.style.opacity = '1';
+  container.style.transform = 'translateY(0)';
+});
